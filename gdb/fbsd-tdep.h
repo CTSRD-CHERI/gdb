6954a951beade372e702675d11a69de7c89216ef
John Baldwin <jhb@FreeBSD.org>
2019-05-07 17:01:28 -0700

    Implement the skip_solib_resolver gdbarch hook for FreeBSD architectures.
    I think this is what makes it so that if you single step into rtld_bind,
    a normal step will actually continue until you step into the function
    being called.

--- gdb/fbsd-tdep.h
+++ gdb/fbsd-tdep.h
@@ -22,6 +22,9 @@
 
 extern void fbsd_init_abi (struct gdbarch_info info, struct gdbarch *gdbarch);
 
+extern CORE_ADDR fbsd_skip_solib_resolver (struct gdbarch *gdbarch,
+					   CORE_ADDR pc);
+
 /* Helper function to generate mappings flags for a single VM map
    entry in 'info proc mappings'.  The KVE_FLAGS and KVE_PROTECTION
    parameters should contain the values of the corresponding fields in
