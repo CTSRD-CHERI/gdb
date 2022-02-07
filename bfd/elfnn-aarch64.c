c2bb10808902d4d0c8fcb7eeb359a3fa1f938b3e
Matthew Malcomson <matthew.malcomson@arm.com>
2022-02-07 16:54:55 +0000

    Error linking binaries with differing e_flags.
    This commit is partly changing two existing (believed buggy) behaviours
    in elfNN_aarch64_merge_private_bfd_data and partly accounting for a
    capability-specific requirement.
    
    The existing behaviours in elfNN_aarch64_merge_private_bfd_data were:
    1) It returned `TRUE` by default.  This effectively ignored the ELF
       flags on the binaries, despite there being code looking at them.
    2) We do not mark the output BFD as initialised until we see flags with
       non-default architecture and flags.  This can't tell the difference
       between linking default objects to non-default objects if the default
       objects are given first on the command line.
    
    The capability-specific requirement is:
    - This function originally returned early if the object file getting
      merged into the existing output object file is not dynamic and has no
      code sections.  The code reasoned that differing ELF flags did not
      matter in this case since there was no code that would be expecting
      it.
      For capabilities the binary compatibility is still important.
      Data sections now contain capabilities as pointers, got sections now
      have a different got element size.
      Hence we avoid this short-circuit if any of the flags we're checking
      are the CHERI_PURECAP flag.

--- bfd/elfnn-aarch64.c
+++ bfd/elfnn-aarch64.c
@@ -8872,7 +8872,7 @@ elfNN_aarch64_merge_private_bfd_data (bfd *ibfd, struct bfd_link_info *info)
   bfd *obfd = info->output_bfd;
   flagword out_flags;
   flagword in_flags;
-  bool flags_compatible = true;
+  bool flags_compatible = false;
   asection *sec;
 
   /* Check if we have the same endianess.  */
@@ -8893,6 +8893,8 @@ elfNN_aarch64_merge_private_bfd_data (bfd *ibfd, struct bfd_link_info *info)
 
   if (!elf_flags_init (obfd))
     {
+      elf_flags_init (obfd) = true;
+
       /* If the input is the default architecture and had the default
 	 flags then do not bother setting the flags for the output
 	 architecture, instead allow future merges to do this.  If no
@@ -8903,7 +8905,6 @@ elfNN_aarch64_merge_private_bfd_data (bfd *ibfd, struct bfd_link_info *info)
 	  && elf_elfheader (ibfd)->e_flags == 0)
 	return true;
 
-      elf_flags_init (obfd) = true;
       elf_elfheader (obfd)->e_flags = in_flags;
 
       if (bfd_get_arch (obfd) == bfd_get_arch (ibfd)
@@ -8927,11 +8928,17 @@ elfNN_aarch64_merge_private_bfd_data (bfd *ibfd, struct bfd_link_info *info)
      Also check to see if there are no code sections in the input.
      In this case there is no need to check for code specific flags.
      XXX - do we need to worry about floating-point format compatability
-     in data sections ?  */
+     in data sections ?
+
+     We definitely need to check for data sections if one set of flags is
+     targetting the PURECAP abi and another is not.  Pointers being
+     capabilities in data sections can not be glossed over.  */
   if (!(ibfd->flags & DYNAMIC))
     {
       bool null_input_bfd = true;
-      bool only_data_sections = true;
+      bool only_data_sections
+	= !(in_flags & EF_AARCH64_CHERI_PURECAP
+	    || out_flags & EF_AARCH64_CHERI_PURECAP);
 
       for (sec = ibfd->sections; sec != NULL; sec = sec->next)
 	{
