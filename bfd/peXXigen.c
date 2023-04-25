8b59f0ec49e66f3782c4e2289ca0f552c65a4f97
John Baldwin <jhb@FreeBSD.org>
2023-04-25 16:27:15 -0700

    bfd peXXigen: Use intptr_t instead of ptrdiff_t to hold a pointer.

--- bfd/peXXigen.c
+++ bfd/peXXigen.c
@@ -2457,7 +2457,7 @@ rsrc_print_section (bfd * abfd, void * vfile)
 	  /* Align data before continuing.  */
 	  int align = (1 << section->alignment_power) - 1;
 
-	  data = (bfd_byte *) (((ptrdiff_t) (data + align)) & ~ align);
+	  data = (bfd_byte *) (((intptr_t) (data + align)) & ~ align);
 	  rva_bias += data - p;
 
 	  /* For reasons that are unclear .rsrc sections are sometimes created
