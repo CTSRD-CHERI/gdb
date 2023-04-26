6a388ac1d71a444803d677ca07cb986b05b69b4f
John Baldwin <jhb@FreeBSD.org>
2023-04-25 17:19:12 -0700

    gnulib: Don't override [u]intptr_t with incorrect values for purecap.

--- gnulib/import/stdint.in.h
+++ gnulib/import/stdint.in.h
@@ -306,7 +306,7 @@ typedef gl_uint_fast32_t gl_uint_fast16_t;
    uintptr_t to avoid conflicting declarations of system functions like
    _findclose in <io.h>.  */
 # if !((defined __KLIBC__ && defined _INTPTR_T_DECLARED) \
-       || defined __MINGW32__)
+       || defined __MINGW32__ || defined __CHERI_PURE_CAPABILITY__)
 #  undef intptr_t
 #  undef uintptr_t
 #  ifdef _WIN64
