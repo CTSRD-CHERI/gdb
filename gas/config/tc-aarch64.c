bminor/binutils-gdb

[AArch64] Initial commit for Morello architecture
The Morello architecture implements support for 129 bit capabilities
to replace traditional pointers to reference memory.  A 129 bit
capability has a 64-bit virtual address in its lowest bits and the
next 64 bits have various access control metadata such as bounds
within which the virtual address can be, permissions and other
metadata for protection.  The top 129th bit is stored out of band and
it indicates if the capability is valid.

Capability registers extends the 64-bit register file and are
similarly numberd c0 to c30.  The stack capability register is csp and
it aliases with sp.  One may access the lower 64 bits of the
capability registers by using the 64-bit register names, i.e. x0-x30
and sp.  The Arm Architecture Reference Manual Supplement Morello for
A-profile Architecture has more details on the register layout.

To ensure backward compatibiility, processors implementing the Morello
architecture can run in two states, the standard A64 and a new state
called C64.  In A64 state, base addresses of memory access
instructions are treated as pointers and traditional aarch64
applications should run out of the box in this state.  In C64 state,
base address registers are expected to be valid capabilities.

There are additional load and store instructions that allow using
capabilities as address registers in A64 mode (and 64-bit registers in
C64 mode).  These are called alternate base loads and stores.

The following new -march flags are implemented:

- a64c: This is the base feature flag to implement instruction
  extensions for Morello that are distinct from its base A64 support.
  Address registers are assumed to be 64-bit except for alternate base
  loads and stores; they are assumed to be capability registers.

- morello: This enables instructions that are allowed on the Morello
  architecture in A64.  This includes armv8.2-a, a64c and other
  extensions that are considered part of the Morello architecture.

gas/config/tc-aarch64.c
@@ -275,6 +275,10 @@ struct reloc_entry
   BASIC_REG_TYPE(FP_S)	/* s[0-31] */	\
   BASIC_REG_TYPE(FP_D)	/* d[0-31] */	\
   BASIC_REG_TYPE(FP_Q)	/* q[0-31] */	\
+  BASIC_REG_TYPE(CA_N)	/* c[0-30] */	\
+  BASIC_REG_TYPE(CA_SP)	/* csp     */ 	\
+  BASIC_REG_TYPE(CA_Z)	/* czr     */ 	\
+  BASIC_REG_TYPE(CA_D)	/* ddc     */ 	\
   BASIC_REG_TYPE(VN)	/* v[0-31] */	\
   BASIC_REG_TYPE(ZN)	/* z[0-31] */	\
   BASIC_REG_TYPE(PN)	/* p[0-15] */	\
@@ -318,6 +322,9 @@ struct reloc_entry
   MULTI_REG_TYPE(R_N, REG_TYPE(R_32) | REG_TYPE(R_64)			\
 		 | REG_TYPE(SP_32) | REG_TYPE(SP_64)			\
 		 | REG_TYPE(Z_32) | REG_TYPE(Z_64))			\
+  /* Typecheck: any capability register (inc CSP) */			\
+  MULTI_REG_TYPE(CA_N_SP, REG_TYPE(CA_N) | REG_TYPE(CA_SP))		\
+  MULTI_REG_TYPE(CA_N_Z, REG_TYPE(CA_N) | REG_TYPE(CA_Z))		\
   /* Pseudo type to mark the end of the enumerator sequence.  */	\
   BASIC_REG_TYPE(MAX)
 
@@ -431,6 +438,18 @@ get_reg_expected_msg (aarch64_reg_type reg_type)
       break;
     case REG_TYPE_PN:
       msg = N_("SVE predicate register expected");
+      break;
+    case REG_TYPE_CA_N:
+      msg = N_("Capability register C0 - C30 expected");
+      break;
+    case REG_TYPE_CA_SP:
+      msg = N_("Capability register CSP expected");
+      break;
+    case REG_TYPE_CA_N_SP:
+      msg = N_("Capability register C0 - C30 or CSP expected");
+      break;
+    case REG_TYPE_CA_Z:
+      msg = N_("Capability register CZR expected");
       break;
     default:
       as_fatal (_("invalid register type %d"), reg_type);
@@ -822,6 +841,11 @@ aarch64_addr_reg_parse (char **ccp, aarch64_reg_type reg_type,
 	  return NULL;
 	}
       str += 2;
+      break;
+
+    case REG_TYPE_CA_N:
+    case REG_TYPE_CA_SP:
+      *qualifier = AARCH64_OPND_QLF_CA;
       break;
 
     default:
@@ -3808,8 +3832,16 @@ static bfd_boolean
 parse_address (char **str, aarch64_opnd_info *operand)
 {
   aarch64_opnd_qualifier_t base_qualifier, offset_qualifier;
+
+  aarch64_reg_type base;
+
+  if (AARCH64_CPU_HAS_FEATURE (cpu_variant, AARCH64_FEATURE_C64))
+    base = REG_TYPE_CA_N_SP;
+  else
+    base = REG_TYPE_R64_SP;
+
   return parse_address_main (str, operand, &base_qualifier, &offset_qualifier,
-			     REG_TYPE_R64_SP, REG_TYPE_R_Z, SHIFTED_NONE);
+			     base, REG_TYPE_R_Z, SHIFTED_NONE);
 }
 
 /* Parse an address in which SVE vector registers and MUL VL are allowed.
@@ -6324,7 +6356,10 @@ parse_operands (char *str, const aarch64_opcode *opcode)
 	    /* Then retry, matching the specific syntax of these addresses.  */
 	    str = start;
 	    po_char_or_fail ('[');
-	    po_reg_or_fail (REG_TYPE_R64_SP);
+	    po_reg_or_fail (AARCH64_CPU_HAS_FEATURE (cpu_variant,
+						     AARCH64_FEATURE_C64)
+			    ? REG_TYPE_CA_N_SP : REG_TYPE_R64_SP);
+
 	    /* Accept optional ", #0".  */
 	    if (operands[i] == AARCH64_OPND_ADDR_SIMPLE
 		&& skip_past_char (&str, ','))
@@ -7278,6 +7313,13 @@ static const reg_entry reg_names[] = {
 
   REGDEF (wzr, 31, Z_32), REGDEF (WZR, 31, Z_32),
   REGDEF (xzr, 31, Z_64), REGDEF (XZR, 31, Z_64),
+
+  /* Capability Registers.  */
+  REGSET31 (c, CA_N), REGSET31 (C, CA_N),
+  REGDEF (csp, 31, CA_SP), REGDEF (CSP, 31, CA_SP),
+  REGDEF (czr, 31, CA_Z), REGDEF (CZR, 31, CA_Z),
+  REGDEF (ddc, 33, CA_D), REGDEF (DDC, 33, CA_D),
+  REGDEF_ALIAS (clr, 30, CA_N), REGDEF_ALIAS (CLR, 30, CA_N),
 
   /* Floating-point single precision registers.  */
   REGSET (s, FP_S), REGSET (S, FP_S),
@@ -9058,6 +9100,7 @@ static const struct aarch64_arch_option_table aarch64_archs[] = {
   {"armv8.5-a", AARCH64_ARCH_V8_5},
   {"armv8.6-a", AARCH64_ARCH_V8_6},
   {"armv8-r",	AARCH64_ARCH_V8_R},
+  {"morello", AARCH64_ARCH_MORELLO},
   {NULL, AARCH64_ARCH_NONE}
 };
 
@@ -9147,6 +9190,10 @@ static const struct aarch64_option_cpu_value_table aarch64_features[] = {
 			AARCH64_FEATURE (AARCH64_FEATURE_SVE, 0)},
   {"f64mm",		AARCH64_FEATURE (AARCH64_FEATURE_F64MM, 0),
 			AARCH64_FEATURE (AARCH64_FEATURE_SVE, 0)},
+  {"a64c",		AARCH64_FEATURE (AARCH64_FEATURE_A64C, 0),
+			AARCH64_ARCH_NONE},
+  {"c64",		AARCH64_FEATURE (AARCH64_FEATURE_C64, 0),
+			AARCH64_FEATURE (AARCH64_FEATURE_A64C, 0)},
   {NULL,		AARCH64_ARCH_NONE, AARCH64_ARCH_NONE},
 };
 

CTSRD-CHERI/gdb

gas/config/tc-aarch64.c
@@ -292,6 +292,10 @@ struct reloc_entry
   BASIC_REG_TYPE(FP_S)	/* s[0-31] */	\
   BASIC_REG_TYPE(FP_D)	/* d[0-31] */	\
   BASIC_REG_TYPE(FP_Q)	/* q[0-31] */	\
+  BASIC_REG_TYPE(CA_N)	/* c[0-30] */	\
+  BASIC_REG_TYPE(CA_SP)	/* csp     */ 	\
+  BASIC_REG_TYPE(CA_Z)	/* czr     */ 	\
+  BASIC_REG_TYPE(CA_D)	/* ddc     */ 	\
   BASIC_REG_TYPE(V)	/* v[0-31] */	\
   BASIC_REG_TYPE(Z)	/* z[0-31] */	\
   BASIC_REG_TYPE(P)	/* p[0-15] */	\
@@ -358,6 +362,9 @@ struct reloc_entry
   MULTI_REG_TYPE(ZA_ZAT, REG_TYPE(ZA) | REG_TYPE(ZAT))			\
   /* A horizontal or vertical slice of a ZA tile.  */			\
   MULTI_REG_TYPE(ZATHV, REG_TYPE(ZATH) | REG_TYPE(ZATV))		\
+  /* Typecheck: any capability register (inc CSP) */			\
+  MULTI_REG_TYPE(CA_N_SP, REG_TYPE(CA_N) | REG_TYPE(CA_SP))		\
+  MULTI_REG_TYPE(CA_N_Z, REG_TYPE(CA_N) | REG_TYPE(CA_Z))		\
   /* Pseudo type to mark the end of the enumerator sequence.  */	\
   END_REG_TYPE(MAX)
 
@@ -508,6 +515,16 @@ get_reg_expected_msg (unsigned int mask, unsigned int seen)
   if (mask == (reg_type_masks[REG_TYPE_Z] | reg_type_masks[REG_TYPE_ZATHV]))
     return N_("expected an SVE vector register or ZA tile slice"
 	      " at operand %d");
+
+  /* Capability registers.  */
+  if (mask == reg_type_masks[REG_TYPE_CA_N])
+    return N_("Capability register C0 - C30 expected");
+  if (mask == reg_type_masks[REG_TYPE_CA_SP])
+    return N_("Capability register CSP expected");
+  if (mask == reg_type_masks[REG_TYPE_CA_N_SP])
+    return N_("Capability register C0 - C30 or CSP expected");
+  if (mask == reg_type_masks[REG_TYPE_CA_Z])
+    return N_("Capability register CZR expected");
 
   return NULL;
 }
@@ -979,6 +996,11 @@ aarch64_addr_reg_parse (char **ccp, aarch64_reg_type reg_type,
 	  return NULL;
 	}
       str += 2;
+      break;
+
+    case REG_TYPE_CA_N:
+    case REG_TYPE_CA_SP:
+      *qualifier = AARCH64_OPND_QLF_CA;
       break;
 
     default:
@@ -4204,8 +4226,16 @@ static bool
 parse_address (char **str, aarch64_opnd_info *operand)
 {
   aarch64_opnd_qualifier_t base_qualifier, offset_qualifier;
+
+  aarch64_reg_type base;
+
+  if (AARCH64_CPU_HAS_FEATURE (cpu_variant, C64))
+    base = REG_TYPE_CA_N_SP;
+  else
+    base = REG_TYPE_R64_SP;
+
   return parse_address_main (str, operand, &base_qualifier, &offset_qualifier,
-			     REG_TYPE_R64_SP, REG_TYPE_R_ZR, SHIFTED_NONE);
+			     base, REG_TYPE_R_ZR, SHIFTED_NONE);
 }
 
 /* Parse an address in which SVE vector registers and MUL VL are allowed.
@@ -7339,7 +7369,9 @@ parse_operands (char *str, const aarch64_opcode *opcode)
 	    /* Then retry, matching the specific syntax of these addresses.  */
 	    str = start;
 	    po_char_or_fail ('[');
-	    po_reg_or_fail (REG_TYPE_R64_SP);
+	    po_reg_or_fail (AARCH64_CPU_HAS_FEATURE (cpu_variant, C64)
+			    ? REG_TYPE_CA_N_SP : REG_TYPE_R64_SP);
+
 	    /* Accept optional ", #0".  */
 	    if (operands[i] == AARCH64_OPND_ADDR_SIMPLE
 		&& skip_past_char (&str, ','))
@@ -8534,6 +8566,13 @@ static const reg_entry reg_names[] = {
 
   REGDEF (wzr, 31, ZR_32), REGDEF (WZR, 31, ZR_32),
   REGDEF (xzr, 31, ZR_64), REGDEF (XZR, 31, ZR_64),
+
+  /* Capability Registers.  */
+  REGSET31 (c, CA_N), REGSET31 (C, CA_N),
+  REGDEF (csp, 31, CA_SP), REGDEF (CSP, 31, CA_SP),
+  REGDEF (czr, 31, CA_Z), REGDEF (CZR, 31, CA_Z),
+  REGDEF (ddc, 33, CA_D), REGDEF (DDC, 33, CA_D),
+  REGDEF_ALIAS (clr, 30, CA_N), REGDEF_ALIAS (CLR, 30, CA_N),
 
   /* Floating-point single precision registers.  */
   REGSET (s, FP_S), REGSET (S, FP_S),
@@ -10288,6 +10327,7 @@ static const struct aarch64_arch_option_table aarch64_archs[] = {
   {"armv9.1-a",	AARCH64_ARCH_FEATURES (V9_1A)},
   {"armv9.2-a",	AARCH64_ARCH_FEATURES (V9_2A)},
   {"armv9.3-a",	AARCH64_ARCH_FEATURES (V9_3A)},
+  {"morello", AARCH64_ARCH_FEATURES (MORELLO)},
   {NULL, AARCH64_NO_FEATURES}
 };
 
@@ -10354,6 +10394,10 @@ static const struct aarch64_option_cpu_value_table aarch64_features[] = {
   {"mops",		AARCH64_FEATURE (MOPS), AARCH64_NO_FEATURES},
   {"hbc",		AARCH64_FEATURE (HBC), AARCH64_NO_FEATURES},
   {"cssc",		AARCH64_FEATURE (CSSC), AARCH64_NO_FEATURES},
+  {"a64c",		AARCH64_FEATURE (A64C),
+			AARCH64_NO_FEATURES},
+  {"c64",		AARCH64_FEATURE (C64),
+			AARCH64_FEATURE (A64C)},
   {NULL,		AARCH64_NO_FEATURES, AARCH64_NO_FEATURES},
 };
 