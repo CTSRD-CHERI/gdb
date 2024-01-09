bminor/binutils-gdb

[Morello] Set LSB for c64 symbols in object code
The Morello processor takes the LSB of the PCC (i.e. the capability
equivalent of PC) as a hint to set PE state to C64 when set and A64
otherwise.

bfd/ChangeLog:

2020-10-20  Siddhesh Poyarekar  <siddesh.poyarekar@arm.com>

	* elfnn-aarch64.c (elfNN_aarch64_final_link_relocate): Set LSB for C64 symbols.
	(elfNN_aarch64_output_map_sym, elfNN_aarch64_output_stub_sym):
	Initialise st_target_internal.
	(aarch64_elfNN_swap_symbol_in, aarch64_elfNN_swap_symbol_out):
	New functions.
	(elfNN_aarch64_size_info): Add them as callbacks.

gas/ChangeLog:

2020-10-20  Siddhesh Poyarekar  <siddesh.poyarekar@arm.com>

	* config/tc-aarch64.c: Include cpu-aarch64.h.
	(IS_C64): New macro.
	(make_mapping_symbol, aarch64_frob_label): Set LSB of C64
	symbol.
	(aarch64_adjust_symtab): Mark all C64 functions.
	(parse_operands): Set LSB when target of ADR is a function.
	(aarch64_fix_adjustable): New function.
	* config/tc-aarch64.h (AARCH64_SET_FLAG, AARCH64_RESET_FLAG,
	AARCH64_FLAG_C64, AARCH64_IS_C64, AARCH64_SET_C64): New
	macros.
	(aarch64_fix_adjustable): New function.
	(tc_fix_adjustable): Use it.

include/ChangeLog:

2020-10-20  Siddhesh Poyarekar  <siddesh.poyarekar@arm.com>

	* elf/aarch64.h (aarch64_st_branch_type): New enum.

opcodes/ChangeLog:

2020-10-20  Siddhesh Poyarekar  <siddesh.poyarekar@arm.com>

	* aarch64-dis.c: Include elf/aarch64.h.
	(get_sym_code_type): Identify C64 functions.

gas/config/tc-aarch64.c
@@ -27,6 +27,7 @@
 #include "safe-ctype.h"
 #include "subsegs.h"
 #include "obstack.h"
+#include "cpu-aarch64.h"
 
 #ifdef OBJ_ELF
 #include "elf/aarch64.h"
@@ -47,6 +48,9 @@
 #define MAP_CUR_INSN (AARCH64_CPU_HAS_FEATURE (cpu_variant,		\
 					       AARCH64_FEATURE_C64)	\
 		      ? MAP_C64 : MAP_INSN)
+
+#define IS_C64 (AARCH64_CPU_HAS_FEATURE (cpu_variant, AARCH64_FEATURE_C64) \
+		? 1 : 0)
 
 static aarch64_feature_set cpu_variant;
 
@@ -1563,6 +1567,11 @@ make_mapping_symbol (enum mstate state, valueT value, fragS * frag)
 
   symbolP = symbol_new (symname, now_seg, frag, value);
   symbol_get_bfdsym (symbolP)->flags |= type | BSF_LOCAL;
+
+  if (state == MAP_C64)
+    AARCH64_SET_C64 (symbolP, 1);
+  else if (state == MAP_INSN)
+    AARCH64_SET_C64 (symbolP, 0);
 
   /* Save the mapping symbols for future reference.  Also check that
      we do not place two mapping symbols at the same offset within a
@@ -6337,6 +6346,21 @@ parse_operands (char *str, const aarch64_opcode *opcode)
 		  case pcreladdr:
 		    gas_assert (operands[i] == AARCH64_OPND_ADDR_PCREL21);
 		    inst.reloc.type = BFD_RELOC_AARCH64_ADR_LO21_PCREL;
+		    if (inst.reloc.exp.X_op == O_symbol
+			&& inst.reloc.exp.X_add_symbol != NULL)
+		      {
+			symbolS *sym = inst.reloc.exp.X_add_symbol;
+
+			/* We set LSB for C64 local functions.  We do not do
+			   this for local labels even in code section because
+			   it could be embedded data.  */
+			if (S_IS_DEFINED (sym) && AARCH64_IS_C64 (sym)
+			    && (symbol_get_bfdsym (sym)->flags & BSF_FUNCTION))
+			  {
+			      inst.reloc.exp.X_add_number += 1;
+			  }
+		      }
+
 		    break;
 		  default:
 		    gas_assert (0);
@@ -7250,6 +7274,8 @@ void
 aarch64_frob_label (symbolS * sym)
 {
   last_label_seen = sym;
+
+  AARCH64_SET_C64 (sym, IS_C64);
 
   dwarf2_emit_label (sym);
 }
@@ -8677,12 +8703,54 @@ check_mapping_symbols (bfd * abfd ATTRIBUTE_UNUSED, asection * sec,
 }
 #endif
 
+/* Avoid relocations from using section symbols in some cases.  */
+bfd_boolean
+aarch64_fix_adjustable (struct fix *fixP)
+{
+  switch (fixP->fx_r_type)
+    {
+      /* A64 <-> C64 transitions are handled by the static linker, so keep
+	 symbol information intact to allow the linker to do something useful
+	 with it.  */
+    case BFD_RELOC_AARCH64_ADR_LO21_PCREL:
+    case BFD_RELOC_AARCH64_ADD_LO12:
+    case BFD_RELOC_AARCH64_BRANCH19:
+    case BFD_RELOC_AARCH64_TSTBR14:
+    case BFD_RELOC_AARCH64_JUMP26:
+    case BFD_RELOC_AARCH64_CALL26:
+      if (AARCH64_IS_C64 (fixP->fx_addsy))
+	return FALSE;
+      break;
+    default:
+      break;
+    }
+
+  return TRUE;
+}
+
 /* Adjust the symbol table.  */
 
 void
 aarch64_adjust_symtab (void)
 {
 #ifdef OBJ_ELF
+  symbolS * sym;
+
+  for (sym = symbol_rootP; sym != NULL; sym = symbol_next (sym))
+    {
+      if (AARCH64_IS_C64 (sym)
+	  && (symbol_get_bfdsym (sym)->flags & BSF_FUNCTION))
+	{
+	  elf_symbol_type * elf_sym;
+
+	  elf_sym = elf_symbol (symbol_get_bfdsym (sym));
+
+	  if (!bfd_is_aarch64_special_symbol_name
+	         (elf_sym->symbol.name, BFD_AARCH64_SPECIAL_SYM_TYPE_ANY))
+	    elf_sym->internal_elf_sym.st_target_internal = ST_BRANCH_TO_C64;
+	}
+    }
+
   /* Remove any overlapping mapping symbols generated by alignment frags.  */
   bfd_map_over_sections (stdoutput, check_mapping_symbols, (char *) 0);
   /* Now do generic ELF adjustments.  */

CTSRD-CHERI/gdb

gas/config/tc-aarch64.c
@@ -27,6 +27,7 @@
 #include "safe-ctype.h"
 #include "subsegs.h"
 #include "obstack.h"
+#include "cpu-aarch64.h"
 
 #ifdef OBJ_ELF
 #include "elf/aarch64.h"
@@ -44,6 +45,9 @@
 
 #define MAP_CUR_INSN (AARCH64_CPU_HAS_FEATURE (cpu_variant, C64)	\
 		      ? MAP_C64 : MAP_INSN)
+
+#define IS_C64 (AARCH64_CPU_HAS_FEATURE (cpu_variant, C64) \
+		? 1 : 0)
 
 static aarch64_feature_set cpu_variant;
 
@@ -1768,6 +1772,11 @@ make_mapping_symbol (enum mstate state, valueT value, fragS * frag)
 
   symbolP = symbol_new (symname, now_seg, frag, value);
   symbol_get_bfdsym (symbolP)->flags |= type | BSF_LOCAL;
+
+  if (state == MAP_C64)
+    AARCH64_SET_C64 (symbolP, 1);
+  else if (state == MAP_INSN)
+    AARCH64_SET_C64 (symbolP, 0);
 
   /* Save the mapping symbols for future reference.  Also check that
      we do not place two mapping symbols at the same offset within a
@@ -7349,6 +7358,21 @@ parse_operands (char *str, const aarch64_opcode *opcode)
 		  case pcreladdr:
 		    gas_assert (operands[i] == AARCH64_OPND_ADDR_PCREL21);
 		    inst.reloc.type = BFD_RELOC_AARCH64_ADR_LO21_PCREL;
+		    if (inst.reloc.exp.X_op == O_symbol
+			&& inst.reloc.exp.X_add_symbol != NULL)
+		      {
+			symbolS *sym = inst.reloc.exp.X_add_symbol;
+
+			/* We set LSB for C64 local functions.  We do not do
+			   this for local labels even in code section because
+			   it could be embedded data.  */
+			if (S_IS_DEFINED (sym) && AARCH64_IS_C64 (sym)
+			    && (symbol_get_bfdsym (sym)->flags & BSF_FUNCTION))
+			  {
+			      inst.reloc.exp.X_add_number += 1;
+			  }
+		      }
+
 		    break;
 		  default:
 		    gas_assert (0);
@@ -8496,6 +8520,8 @@ void
 aarch64_frob_label (symbolS * sym)
 {
   last_label_seen = sym;
+
+  AARCH64_SET_C64 (sym, IS_C64);
 
   dwarf2_emit_label (sym);
 }
@@ -9928,12 +9954,54 @@ check_mapping_symbols (bfd * abfd ATTRIBUTE_UNUSED, asection * sec,
 }
 #endif
 
+/* Avoid relocations from using section symbols in some cases.  */
+bool
+aarch64_fix_adjustable (struct fix *fixP)
+{
+  switch (fixP->fx_r_type)
+    {
+      /* A64 <-> C64 transitions are handled by the static linker, so keep
+	 symbol information intact to allow the linker to do something useful
+	 with it.  */
+    case BFD_RELOC_AARCH64_ADR_LO21_PCREL:
+    case BFD_RELOC_AARCH64_ADD_LO12:
+    case BFD_RELOC_AARCH64_BRANCH19:
+    case BFD_RELOC_AARCH64_TSTBR14:
+    case BFD_RELOC_AARCH64_JUMP26:
+    case BFD_RELOC_AARCH64_CALL26:
+      if (AARCH64_IS_C64 (fixP->fx_addsy))
+	return false;
+      break;
+    default:
+      break;
+    }
+
+  return true;
+}
+
 /* Adjust the symbol table.  */
 
 void
 aarch64_adjust_symtab (void)
 {
 #ifdef OBJ_ELF
+  symbolS * sym;
+
+  for (sym = symbol_rootP; sym != NULL; sym = symbol_next (sym))
+    {
+      if (AARCH64_IS_C64 (sym)
+	  && (symbol_get_bfdsym (sym)->flags & BSF_FUNCTION))
+	{
+	  elf_symbol_type * elf_sym;
+
+	  elf_sym = elf_symbol (symbol_get_bfdsym (sym));
+
+	  if (!bfd_is_aarch64_special_symbol_name
+	         (elf_sym->symbol.name, BFD_AARCH64_SPECIAL_SYM_TYPE_ANY))
+	    elf_sym->internal_elf_sym.st_target_internal = ST_BRANCH_TO_C64;
+	}
+    }
+
   /* Remove any overlapping mapping symbols generated by alignment frags.  */
   bfd_map_over_sections (stdoutput, check_mapping_symbols, (char *) 0);
   /* Now do generic ELF adjustments.  */