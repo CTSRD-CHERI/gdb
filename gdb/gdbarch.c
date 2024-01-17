bminor/binutils-gdb

Add gdbarch methods for printing capabilities.
gdbarch_print_cap prints a full capability in either verbose or
compat forms.

gdbarch_print_cap_attributes prints just the metadata such as bounds
and permissions of a capability without the address.

gdb/gdbarch.c
@@ -132,6 +132,8 @@ struct gdbarch
   gdbarch_remote_register_number_ftype *remote_register_number;
   gdbarch_get_cap_tag_from_address_ftype *get_cap_tag_from_address;
   gdbarch_set_cap_tag_from_address_ftype *set_cap_tag_from_address;
+  gdbarch_print_cap_ftype *print_cap;
+  gdbarch_print_cap_attributes_ftype *print_cap_attributes;
   gdbarch_fetch_tls_load_module_address_ftype *fetch_tls_load_module_address;
   gdbarch_get_thread_local_address_ftype *get_thread_local_address;
   CORE_ADDR frame_args_skip;
@@ -330,6 +332,8 @@ gdbarch_alloc (const struct gdbarch_info *info,
   gdbarch->remote_register_number = default_remote_register_number;
   gdbarch->get_cap_tag_from_address = default_get_cap_tag_from_address;
   gdbarch->set_cap_tag_from_address = default_set_cap_tag_from_address;
+  gdbarch->print_cap = default_print_cap;
+  gdbarch->print_cap_attributes = default_print_cap_attributes;
   gdbarch->unwind_pc = default_unwind_pc;
   gdbarch->unwind_sp = default_unwind_sp;
   gdbarch->stabs_argument_has_addr = default_stabs_argument_has_addr;
@@ -497,6 +501,8 @@ verify_gdbarch (struct gdbarch *gdbarch)
   /* Skip verify of remote_register_number, invalid_p == 0 */
   /* Skip verify of get_cap_tag_from_address, invalid_p == 0 */
   /* Skip verify of set_cap_tag_from_address, invalid_p == 0 */
+  /* Skip verify of print_cap, invalid_p == 0 */
+  /* Skip verify of print_cap_attributes, invalid_p == 0 */
   /* Skip verify of fetch_tls_load_module_address, has predicate.  */
   /* Skip verify of get_thread_local_address, has predicate.  */
   /* Skip verify of frame_args_skip, invalid_p == 0 */
@@ -952,6 +958,12 @@ gdbarch_dump (struct gdbarch *gdbarch, struct ui_file *file)
   fprintf_filtered (file,
                       "gdbarch_dump: set_cap_tag_from_address = <%s>\n",
                       host_address_to_string (gdbarch->set_cap_tag_from_address));
+  fprintf_filtered (file,
+                      "gdbarch_dump: print_cap = <%s>\n",
+                      host_address_to_string (gdbarch->print_cap));
+  fprintf_filtered (file,
+                      "gdbarch_dump: print_cap_attributes = <%s>\n",
+                      host_address_to_string (gdbarch->print_cap_attributes));
   fprintf_filtered (file,
                       "gdbarch_dump: gdbarch_fetch_tls_load_module_address_p() = %d\n",
                       gdbarch_fetch_tls_load_module_address_p (gdbarch));
@@ -3024,6 +3036,40 @@ set_gdbarch_set_cap_tag_from_address (struct gdbarch *gdbarch,
                                       gdbarch_set_cap_tag_from_address_ftype set_cap_tag_from_address)
 {
   gdbarch->set_cap_tag_from_address = set_cap_tag_from_address;
+}
+
+void
+gdbarch_print_cap (struct gdbarch *gdbarch, const gdb_byte *contents, bool tag, bool compact, struct ui_file *stream)
+{
+  gdb_assert (gdbarch != NULL);
+  gdb_assert (gdbarch->print_cap != NULL);
+  if (gdbarch_debug >= 2)
+    fprintf_unfiltered (gdb_stdlog, "gdbarch_print_cap called\n");
+  gdbarch->print_cap (gdbarch, contents, tag, compact, stream);
+}
+
+void
+set_gdbarch_print_cap (struct gdbarch *gdbarch,
+                       gdbarch_print_cap_ftype print_cap)
+{
+  gdbarch->print_cap = print_cap;
+}
+
+void
+gdbarch_print_cap_attributes (struct gdbarch *gdbarch, const gdb_byte *contents, bool tag, struct ui_file *stream)
+{
+  gdb_assert (gdbarch != NULL);
+  gdb_assert (gdbarch->print_cap_attributes != NULL);
+  if (gdbarch_debug >= 2)
+    fprintf_unfiltered (gdb_stdlog, "gdbarch_print_cap_attributes called\n");
+  gdbarch->print_cap_attributes (gdbarch, contents, tag, stream);
+}
+
+void
+set_gdbarch_print_cap_attributes (struct gdbarch *gdbarch,
+                                  gdbarch_print_cap_attributes_ftype print_cap_attributes)
+{
+  gdbarch->print_cap_attributes = print_cap_attributes;
 }
 
 bool

CTSRD-CHERI/gdb

gdb/gdbarch.c
@@ -135,6 +135,8 @@ struct gdbarch
   gdbarch_remote_register_number_ftype *remote_register_number = default_remote_register_number;
   gdbarch_get_cap_tag_from_address_ftype *get_cap_tag_from_address = default_get_cap_tag_from_address;
   gdbarch_set_cap_tag_from_address_ftype *set_cap_tag_from_address = default_set_cap_tag_from_address;
+  gdbarch_print_cap_ftype *print_cap = default_print_cap;
+  gdbarch_print_cap_attributes_ftype *print_cap_attributes = default_print_cap_attributes;
   gdbarch_fetch_tls_load_module_address_ftype *fetch_tls_load_module_address = nullptr;
   gdbarch_get_thread_local_address_ftype *get_thread_local_address = nullptr;
   CORE_ADDR frame_args_skip = 0;
@@ -404,6 +406,8 @@ verify_gdbarch (struct gdbarch *gdbarch)
   /* Skip verify of remote_register_number, invalid_p == 0 */
   /* Skip verify of get_cap_tag_from_address, invalid_p == 0 */
   /* Skip verify of set_cap_tag_from_address, invalid_p == 0 */
+  /* Skip verify of print_cap, invalid_p == 0 */
+  /* Skip verify of print_cap_attributes, invalid_p == 0 */
   /* Skip verify of fetch_tls_load_module_address, has predicate.  */
   /* Skip verify of get_thread_local_address, has predicate.  */
   /* Skip verify of frame_args_skip, invalid_p == 0 */
@@ -881,6 +885,12 @@ gdbarch_dump (struct gdbarch *gdbarch, struct ui_file *file)
   gdb_printf (file,
 	      "gdbarch_dump: set_cap_tag_from_address = <%s>\n",
 	      host_address_to_string (gdbarch->set_cap_tag_from_address));
+  gdb_printf (file,
+	      "gdbarch_dump: print_cap = <%s>\n",
+	      host_address_to_string (gdbarch->print_cap));
+  gdb_printf (file,
+	      "gdbarch_dump: print_cap_attributes = <%s>\n",
+	      host_address_to_string (gdbarch->print_cap_attributes));
   gdb_printf (file,
 	      "gdbarch_dump: gdbarch_fetch_tls_load_module_address_p() = %d\n",
 	      gdbarch_fetch_tls_load_module_address_p (gdbarch));
@@ -3049,6 +3059,40 @@ set_gdbarch_set_cap_tag_from_address (struct gdbarch *gdbarch,
 				      gdbarch_set_cap_tag_from_address_ftype set_cap_tag_from_address)
 {
   gdbarch->set_cap_tag_from_address = set_cap_tag_from_address;
+}
+
+void
+gdbarch_print_cap (struct gdbarch *gdbarch, const gdb_byte *contents, bool tag, bool compact, struct ui_file *stream)
+{
+  gdb_assert (gdbarch != NULL);
+  gdb_assert (gdbarch->print_cap != NULL);
+  if (gdbarch_debug >= 2)
+    gdb_printf (gdb_stdlog, "gdbarch_print_cap called\n");
+  gdbarch->print_cap (gdbarch, contents, tag, compact, stream);
+}
+
+void
+set_gdbarch_print_cap (struct gdbarch *gdbarch,
+		       gdbarch_print_cap_ftype print_cap)
+{
+  gdbarch->print_cap = print_cap;
+}
+
+void
+gdbarch_print_cap_attributes (struct gdbarch *gdbarch, const gdb_byte *contents, bool tag, struct ui_file *stream)
+{
+  gdb_assert (gdbarch != NULL);
+  gdb_assert (gdbarch->print_cap_attributes != NULL);
+  if (gdbarch_debug >= 2)
+    gdb_printf (gdb_stdlog, "gdbarch_print_cap_attributes called\n");
+  gdbarch->print_cap_attributes (gdbarch, contents, tag, stream);
+}
+
+void
+set_gdbarch_print_cap_attributes (struct gdbarch *gdbarch,
+				  gdbarch_print_cap_attributes_ftype print_cap_attributes)
+{
+  gdbarch->print_cap_attributes = print_cap_attributes;
 }
 
 bool