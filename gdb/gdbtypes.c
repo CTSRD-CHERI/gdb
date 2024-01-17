bminor/binutils-gdb

Support data_capability and code_capability types + update target description
Support the data_capability and code_capability types, which the capability
counterparts of the data_ptr and code_ptr types.

Adjust the Morello C registers to be of data_capability and code_capability
types.

gdb/ChangeLog:

2020-11-11  Luis Machado  <luis.machado@arm.com>

	* aarch64-tdep.c (aarch64_address_class_type_flags)
	(aarch64_address_class_type_flags_to_name)
	(aarch64_address_class_name_to_type_flags): Use
	TYPE_INSTANCE_FLAG_CAPABILITY instead of
	TYPE_INSTANCE_FLAG_ADDRESS_CLASS_1.
	(aarch64_gdbarch_init): Set capability size to 128.
	* features/aarch64-capability.c: Regenerate.
	* features/aarch64-capability.xml: Update C register types.
	* gdbarch.c: Regenerate.
	* gdbarch.h: Regenerate.
	* gdbarch.sh (code_capability_bit, data_capability)
	(dwarf2_capability_size): Remove.
	* gdbtypes.c (gdbtypes_post_init): Initialize data_capability and
	code_capability types.
	* gdbtypes.h (TYPE_INSTANCE_FLAG_ADDRESS_CLASS_ALL): Include
	TYPE_INSTANCE_FLAG_CAPABILITY in the list of address classes.
	(struct builtin_type) <builtin_data_addr_capability>: Rename to
	builtin_data_capability.
	(struct builtin_type) <builtin_code_addr_capability>: Rename to
	builtin_code_capability.
	* target-descriptions.c (make_gdb_type): Update type names.

gdbsupport/ChangeLog:

2020-11-11  Luis Machado  <luis.machado@arm.com>

	* tdesc.cc (tdesc_predefined_types): Update type names for
	capabilities.

gdb/gdbtypes.c
@@ -5925,15 +5925,26 @@ gdbtypes_post_init (struct gdbarch *gdbarch)
 
   /* Capability types.  */
   builtin_type->builtin_intcap_t
-    = arch_capability_type (gdbarch, 128, 0, "__intcap_t");
+    = arch_capability_type (gdbarch, gdbarch_capability_bit (gdbarch), 0,
+			    "__intcap_t");
   builtin_type->builtin_uintcap_t
-    = arch_capability_type (gdbarch, 128, 1, "__uintcap_t");
+    = arch_capability_type (gdbarch, gdbarch_capability_bit (gdbarch), 1,
+			    "__uintcap_t");
 
   /* Capability pointer types.  */
-  builtin_type->builtin_data_addr_capability
-    = lookup_pointer_type (builtin_type->builtin_void);
-  builtin_type->builtin_code_addr_capability
-    = lookup_pointer_type (lookup_function_type (builtin_type->builtin_void));
+  builtin_type->builtin_data_capability
+    = arch_pointer_type (gdbarch, gdbarch_capability_bit (gdbarch), "",
+			 builtin_type->builtin_void);
+  builtin_type->builtin_data_capability->set_instance_flags
+    (builtin_type->builtin_data_capability->instance_flags ()
+     | TYPE_INSTANCE_FLAG_CAPABILITY);
+
+  builtin_type->builtin_code_capability
+    = arch_pointer_type (gdbarch, gdbarch_capability_bit (gdbarch), "",
+			 lookup_function_type (builtin_type->builtin_void));
+  builtin_type->builtin_code_capability->set_instance_flags
+    (builtin_type->builtin_code_capability->instance_flags ()
+     | TYPE_INSTANCE_FLAG_CAPABILITY);
 
   /* This type represents a GDB internal function.  */
   builtin_type->internal_fn

CTSRD-CHERI/gdb

gdb/gdbtypes.c
@@ -6144,15 +6144,26 @@ create_gdbtypes_data (struct gdbarch *gdbarch)
 
   /* Capability types.  */
   builtin_type->builtin_intcap_t
-    = init_capability_type (alloc, 128, 0, "__intcap_t");
+    = init_capability_type (alloc, gdbarch_capability_bit (gdbarch), 0,
+			    "__intcap_t");
   builtin_type->builtin_uintcap_t
-    = init_capability_type (alloc, 128, 1, "__uintcap_t");
+    = init_capability_type (alloc, gdbarch_capability_bit (gdbarch), 1,
+			    "__uintcap_t");
 
   /* Capability pointer types.  */
-  builtin_type->builtin_data_addr_capability
-    = lookup_pointer_type (builtin_type->builtin_void);
-  builtin_type->builtin_code_addr_capability
-    = lookup_pointer_type (lookup_function_type (builtin_type->builtin_void));
+  builtin_type->builtin_data_capability
+    = init_pointer_type (alloc, gdbarch_capability_bit (gdbarch), "",
+			 builtin_type->builtin_void);
+  builtin_type->builtin_data_capability->set_instance_flags
+    (builtin_type->builtin_data_capability->instance_flags ()
+     | TYPE_INSTANCE_FLAG_CAPABILITY);
+
+  builtin_type->builtin_code_capability
+    = init_pointer_type (alloc, gdbarch_capability_bit (gdbarch), "",
+			 lookup_function_type (builtin_type->builtin_void));
+  builtin_type->builtin_code_capability->set_instance_flags
+    (builtin_type->builtin_code_capability->instance_flags ()
+     | TYPE_INSTANCE_FLAG_CAPABILITY);
 
   /* This type represents a GDB internal function.  */
   builtin_type->internal_fn