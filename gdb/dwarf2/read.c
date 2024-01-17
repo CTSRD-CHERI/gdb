bminor/binutils-gdb

[General] More capability type handling (merge with others)
Teach more parts of GDB how to handle capabilities properly, add a function
to print capabilities in their natural format and initialize capability types
properly.

gdb/ChangeLog:

2020-10-20  Luis Machado  <luis.machado@arm.com>

	* c-typeprint.c (c_type_print_varspec_prefix)
	(c_type_print_varspec_suffix): Handle capability type.
	* dwarf2/read.c (read_base_type): Call init_capability_type for
	capabilities.
	* gdbtypes.c (init_capability_type): New function.
	(type_align): Handle capability type.
	(recursive_dump_type): Likewise.
	(arch_capability_type): New function.
	(gdbtypes_post_init): Call arch_capability_type for capability
	types.
	* gdbtypes.h (init_capability_type, arch_capability_type):  New
	prototypes.
	* valprint.c: Include gdbsupport/capability.h.
	(generic_value_print_capability): New function.
	(generic_value_print): Handle capability types.

gdb/dwarf2/read.c
@@ -18025,13 +18025,11 @@ read_base_type (struct die_info *die, struct dwarf2_cu *cu)
 	break;
 
       case DW_ATE_CHERI_signed_intcap:
-      case DW_ATE_CHERI_unsigned_intcap:
-	{
-	  /* Turn DW_ATE_CHERI_*_intcap into a void * pointer.  */
-	  type = init_type (objfile, TYPE_CODE_VOID, TARGET_CHAR_BIT, NULL);
-	  type = init_pointer_type (objfile, bits, name, type);
+	  type = init_capability_type (objfile, bits, false, name);
 	  break;
-	}
+      case DW_ATE_CHERI_unsigned_intcap:
+	  type = init_capability_type (objfile, bits, true, name);
+	  break;
 
       default:
 	complaint (_("unsupported DW_AT_encoding: '%s'"),

gdb/gdbtypes.c
@@ -3341,6 +3341,22 @@ init_pointer_type (struct objfile *objfile,
   return t;
 }
 
+/* Allocate a TYPE_CODE_CAPABILITY type structure associated with OBJFILE.
+   BIT is the type size in bits.  If UNSIGNED_P is non-zero, set
+   the type's TYPE_UNSIGNED flag.  NAME is the type name.  */
+
+struct type *
+init_capability_type (struct objfile *objfile,
+		      int bit, bool unsigned_p, const char *name)
+{
+  struct type *t;
+
+  t = init_type (objfile, TYPE_CODE_CAPABILITY, bit, name);
+  t->set_is_unsigned (unsigned_p);
+
+  return t;
+}
+
 /* See gdbtypes.h.  */
 
 unsigned
@@ -3370,6 +3386,7 @@ type_align (struct type *type)
   switch (type->code ())
     {
     case TYPE_CODE_PTR:
+    case TYPE_CODE_CAPABILITY:
     case TYPE_CODE_FUNC:
     case TYPE_CODE_FLAGS:
     case TYPE_CODE_INT:
@@ -5014,6 +5031,9 @@ recursive_dump_type (struct type *type, int spaces)
     case TYPE_CODE_NAMESPACE:
       printf_filtered ("(TYPE_CODE_NAMESPACE)");
       break;
+    case TYPE_CODE_CAPABILITY:
+      printf_filtered ("(TYPE_CODE_CAPABILITY)");
+      break;
     default:
       printf_filtered ("(UNKNOWN TYPE CODE)");
       break;
@@ -5603,6 +5623,21 @@ arch_pointer_type (struct gdbarch *gdbarch,
   return t;
 }
 
+/* Allocate a TYPE_CODE_CAPABILITY type structure associated with GDBARCH.
+   BIT is the type size in bits.  If UNSIGNED_P is non-zero, set
+   the type's TYPE_UNSIGNED flag.  NAME is the type name.  */
+
+struct type *
+arch_capability_type (struct gdbarch *gdbarch,
+		      int bit, bool unsigned_p, const char *name)
+{
+  struct type *t;
+
+  t = arch_type (gdbarch, TYPE_CODE_CAPABILITY, bit, name);
+  t->set_is_unsigned (unsigned_p);
+  return t;
+}
+
 /* Allocate a TYPE_CODE_FLAGS type structure associated with GDBARCH.
    NAME is the type name.  BIT is the size of the flag word in bits.  */
 
@@ -5890,9 +5925,9 @@ gdbtypes_post_init (struct gdbarch *gdbarch)
 
   /* Capability types.  */
   builtin_type->builtin_intcap_t
-    = arch_integer_type (gdbarch, 128, 0, "__intcap_t");
+    = arch_capability_type (gdbarch, 128, 0, "__intcap_t");
   builtin_type->builtin_uintcap_t
-    = arch_integer_type (gdbarch, 128, 1, "__uintcap_t");
+    = arch_capability_type (gdbarch, 128, 1, "__uintcap_t");
 
   /* Capability pointer types.  */
   builtin_type->builtin_data_addr_capability

CTSRD-CHERI/gdb

gdb/dwarf2/read.c
@@ -15393,13 +15393,11 @@ read_base_type (struct die_info *die, struct dwarf2_cu *cu)
 	break;
 
       case DW_ATE_CHERI_signed_intcap:
-      case DW_ATE_CHERI_unsigned_intcap:
-	{
-	  /* Turn DW_ATE_CHERI_*_intcap into a void * pointer.  */
-	  type = alloc.new_type (TYPE_CODE_VOID, TARGET_CHAR_BIT, NULL);
-	  type = init_pointer_type (alloc, bits, name, type);
+	  type = init_capability_type (alloc, bits, false, name);
 	  break;
-	}
+      case DW_ATE_CHERI_unsigned_intcap:
+	  type = init_capability_type (alloc, bits, true, name);
+	  break;
 
       default:
 	complaint (_("unsupported DW_AT_encoding: '%s'"),

gdb/gdbtypes.c
@@ -3531,6 +3531,20 @@ init_fixed_point_type (type_allocator &alloc,
 
 /* See gdbtypes.h.  */
 
+struct type *
+init_capability_type (type_allocator &alloc,
+		      int bit, bool unsigned_p, const char *name)
+{
+  struct type *t;
+
+  t = alloc.new_type (TYPE_CODE_CAPABILITY, bit, name);
+  t->set_is_unsigned (unsigned_p);
+
+  return t;
+}
+
+/* See gdbtypes.h.  */
+
 unsigned
 type_raw_align (struct type *type)
 {
@@ -3557,6 +3571,7 @@ type_align (struct type *type)
   switch (type->code ())
     {
     case TYPE_CODE_PTR:
+    case TYPE_CODE_CAPABILITY:
     case TYPE_CODE_FUNC:
     case TYPE_CODE_FLAGS:
     case TYPE_CODE_INT:
@@ -5231,6 +5246,9 @@ recursive_dump_type (struct type *type, int spaces)
     case TYPE_CODE_FIXED_POINT:
       gdb_printf ("(TYPE_CODE_FIXED_POINT)");
       break;
+    case TYPE_CODE_CAPABILITY:
+      gdb_printf ("(TYPE_CODE_CAPABILITY)");
+      break;
     default:
       gdb_printf ("(UNKNOWN TYPE CODE)");
       break;
@@ -6126,9 +6144,9 @@ create_gdbtypes_data (struct gdbarch *gdbarch)
 
   /* Capability types.  */
   builtin_type->builtin_intcap_t
-    = init_integer_type (alloc, 128, 0, "__intcap_t");
+    = init_capability_type (alloc, 128, 0, "__intcap_t");
   builtin_type->builtin_uintcap_t
-    = init_integer_type (alloc, 128, 1, "__uintcap_t");
+    = init_capability_type (alloc, 128, 1, "__uintcap_t");
 
   /* Capability pointer types.  */
   builtin_type->builtin_data_addr_capability