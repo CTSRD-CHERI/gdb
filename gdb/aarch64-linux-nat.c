bminor/binutils-gdb

Read capability tags from memory
This patch enables capability reads from memory, including the tag bit.

It enables both native GDB and gdbserver to do so.

gdb/ChangeLog:

2021-01-15  Luis Machado  <luis.machado@arm.com>

	* aarch64-linux-nat.c (aarch64_linux_nat_target)
	<read_capability>: New function override.
	(aarch64_linux_nat_target::read_capability): New function.
	(maint_print_cap_from_addr_cmd): New function.
	(add_show_debug_regs_command): Register new maintenance command.
	* aarch64-linux-tdep.c: Include target.h.
	(aarch64_linux_get_cap_tag_from_address): New function.
	(aarch64_linux_init_abi): Register hook for
	gdbarch_get_cap_tag_from_address
	* arch-utils.c (default_get_cap_tag_from_address): New function.
	* arch-utils.h (default_get_cap_tag_from_address): New prototype.
	* configure.nat: Add nat/aarch64-cap-linux.o to the list of object
	files for AArch64-Linux.
	* gdbarch.c: Regenerate.
	* gdbarch.h: Regenerate.
	* gdbarch.sh (get_cap_tag_from_address): New gdbarch hook.
	* nat/aarch64-cap-linux.c: New file.
	* nat/aarch64-cap-linux.h (aarch64_linux_read_capability): New
	prototype.
	* remote.c (remote_target) <read_capability>: New function override.
	(PACKET_qXfer_capability): New enum.
	(remote_target::xfer_partial): Handle TARGET_OBJECT_CAPABILITY.
	(remote_target::read_capability): New member function.
	(_initialize_remote): Add new packet configuration.
	* target-delegates.c: Regenerate.
	* target.c (target_read_capability): New function.
	* target.h (target_object) <TARGET_OBJECT_CAPABILITY>: New enum field.
	(struct target_ops) <read_capability>: New virtual member function.
	(target_read_capability): New prototype.
	* valprint.c (generic_value_print_capability): Handle tags from
	capabilities stored in memory.
	* value.c (value_fetch_lazy_memory): Fetch tag if reading a capability.

gdbserver/ChangeLog

2021-01-15  Luis Machado  <luis.machado@arm.com>

	* configure.srv: Add nat/aarch64-cap-linux.o to the list of object
	files for AArc

gdb/aarch64-linux-nat.c
@@ -102,6 +102,7 @@ public:
     override;
 
   struct gdbarch *thread_architecture (ptid_t) override;
+  gdb::byte_vector read_capability (CORE_ADDR addr) override;
 };
 
 static aarch64_linux_nat_target the_aarch64_linux_nat_target;
@@ -1056,6 +1057,40 @@ aarch64_linux_nat_target::thread_architecture (ptid_t ptid)
   return gdbarch_find_by_info (info);
 }
 
+/* Implement the "read_capability" target_ops method.  */
+
+gdb::byte_vector
+aarch64_linux_nat_target::read_capability (CORE_ADDR addr)
+{
+  int tid = get_ptrace_pid (inferior_ptid);
+
+  struct user_cap cap;
+
+  if (!aarch64_linux_read_capability (tid, addr, cap))
+    perror_with_name (_("Unable to read capability from address."));
+
+  gdb::byte_vector cap_vec (17);
+  memcpy (cap_vec.data (), &cap.tag, 1);
+  memcpy (cap_vec.data () + 1, &cap.val, 16);
+
+  return cap_vec;
+}
+
+/* Implement the maintenance print capability tag command.  */
+
+static void
+maint_print_cap_from_addr_cmd (const char *args, int from_tty)
+{
+  gdb::byte_vector cap;
+  CORE_ADDR addr = parse_and_eval_address (args);
+  cap = target_read_capability (addr);
+
+  for (auto it : cap)
+    fprintf_unfiltered (gdb_stdlog, "%x ", it);
+
+  fputs_unfiltered ("\n", gdb_stdlog);
+}
+
 /* Define AArch64 maintenance commands.  */
 
 static void
@@ -1075,6 +1110,10 @@ triggers a breakpoint or watchpoint."),
 			   NULL,
 			   &maintenance_set_cmdlist,
 			   &maintenance_show_cmdlist);
+
+  add_cmd ("cap_from_addr", class_maintenance, maint_print_cap_from_addr_cmd, _("\
+Print the capability from addr."),
+		 &maintenanceprintlist);
 }
 
 void _initialize_aarch64_linux_nat ();

gdb/target-delegates.c
@@ -173,6 +173,7 @@ struct dummy_target : public target_ops
   const struct frame_unwind *get_tailcall_unwinder () override;
   void prepare_to_generate_core () override;
   void done_generating_core () override;
+  gdb::byte_vector read_capability (CORE_ADDR arg0) override;
 };
 
 struct debug_target : public target_ops
@@ -344,6 +345,7 @@ struct debug_target : public target_ops
   const struct frame_unwind *get_tailcall_unwinder () override;
   void prepare_to_generate_core () override;
   void done_generating_core () override;
+  gdb::byte_vector read_capability (CORE_ADDR arg0) override;
 };
 
 void
@@ -4411,5 +4413,31 @@ debug_target::done_generating_core ()
   this->beneath ()->done_generating_core ();
   fprintf_unfiltered (gdb_stdlog, "<- %s->done_generating_core (", this->beneath ()->shortname ());
   fputs_unfiltered (")\n", gdb_stdlog);
+}
+
+gdb::byte_vector
+target_ops::read_capability (CORE_ADDR arg0)
+{
+  return this->beneath ()->read_capability (arg0);
+}
+
+gdb::byte_vector
+dummy_target::read_capability (CORE_ADDR arg0)
+{
+  tcomplain ();
+}
+
+gdb::byte_vector
+debug_target::read_capability (CORE_ADDR arg0)
+{
+  gdb::byte_vector result;
+  fprintf_unfiltered (gdb_stdlog, "-> %s->read_capability (...)\n", this->beneath ()->shortname ());
+  result = this->beneath ()->read_capability (arg0);
+  fprintf_unfiltered (gdb_stdlog, "<- %s->read_capability (", this->beneath ()->shortname ());
+  target_debug_print_CORE_ADDR (arg0);
+  fputs_unfiltered (") = ", gdb_stdlog);
+  target_debug_print_gdb_byte_vector (result);
+  fputs_unfiltered ("\n", gdb_stdlog);
+  return result;
 }
 

CTSRD-CHERI/gdb

gdb/aarch64-linux-nat.c
@@ -113,6 +113,8 @@ public:
   /* Write allocation tags to memory via PTRACE.  */
   bool store_memtags (CORE_ADDR address, size_t len,
 		      const gdb::byte_vector &tags, int type) override;
+
+  gdb::byte_vector read_capability (CORE_ADDR addr) override;
 };
 
 static aarch64_linux_nat_target the_aarch64_linux_nat_target;
@@ -1157,11 +1159,49 @@ aarch64_linux_nat_target::store_memtags (CORE_ADDR address, size_t len,
   return false;
 }
 
+/* Implement the "read_capability" target_ops method.  */
+
+gdb::byte_vector
+aarch64_linux_nat_target::read_capability (CORE_ADDR addr)
+{
+  int tid = get_ptrace_pid (inferior_ptid);
+
+  struct user_cap cap;
+
+  if (!aarch64_linux_read_capability (tid, addr, cap))
+    perror_with_name (_("Unable to read capability from address."));
+
+  gdb::byte_vector cap_vec (17);
+  memcpy (cap_vec.data (), &cap.tag, 1);
+  memcpy (cap_vec.data () + 1, &cap.val, 16);
+
+  return cap_vec;
+}
+
+/* Implement the maintenance print capability tag command.  */
+
+static void
+maint_print_cap_from_addr_cmd (const char *args, int from_tty)
+{
+  gdb::byte_vector cap;
+  CORE_ADDR addr = parse_and_eval_address (args);
+  cap = target_read_capability (addr);
+
+  for (auto it : cap)
+    gdb_printf (gdb_stdlog, "%x ", it);
+
+  gdb_puts ("\n", gdb_stdlog);
+}
+
 void _initialize_aarch64_linux_nat ();
 void
 _initialize_aarch64_linux_nat ()
 {
   aarch64_initialize_hw_point ();
+
+  add_cmd ("cap_from_addr", class_maintenance, maint_print_cap_from_addr_cmd, _("\
+Print the capability from addr."),
+	   &maintenanceprintlist);
 
   /* Register the target.  */
   linux_target = &the_aarch64_linux_nat_target;

gdb/target-delegates.c
@@ -197,6 +197,7 @@ struct dummy_target : public target_ops
   bool fetch_memtags (CORE_ADDR arg0, size_t arg1, gdb::byte_vector &arg2, int arg3) override;
   bool store_memtags (CORE_ADDR arg0, size_t arg1, const gdb::byte_vector &arg2, int arg3) override;
   x86_xsave_layout fetch_x86_xsave_layout () override;
+  gdb::byte_vector read_capability (CORE_ADDR arg0) override;
 };
 
 struct debug_target : public target_ops
@@ -372,6 +373,7 @@ struct debug_target : public target_ops
   bool fetch_memtags (CORE_ADDR arg0, size_t arg1, gdb::byte_vector &arg2, int arg3) override;
   bool store_memtags (CORE_ADDR arg0, size_t arg1, const gdb::byte_vector &arg2, int arg3) override;
   x86_xsave_layout fetch_x86_xsave_layout () override;
+  gdb::byte_vector read_capability (CORE_ADDR arg0) override;
 };
 
 void
@@ -4559,6 +4561,32 @@ debug_target::fetch_x86_xsave_layout ()
   gdb_printf (gdb_stdlog, "<- %s->fetch_x86_xsave_layout (", this->beneath ()->shortname ());
   gdb_puts (") = ", gdb_stdlog);
   target_debug_print_x86_xsave_layout (result);
+  gdb_puts ("\n", gdb_stdlog);
+  return result;
+}
+
+gdb::byte_vector
+target_ops::read_capability (CORE_ADDR arg0)
+{
+  return this->beneath ()->read_capability (arg0);
+}
+
+gdb::byte_vector
+dummy_target::read_capability (CORE_ADDR arg0)
+{
+  tcomplain ();
+}
+
+gdb::byte_vector
+debug_target::read_capability (CORE_ADDR arg0)
+{
+  gdb_printf (gdb_stdlog, "-> %s->read_capability (...)\n", this->beneath ()->shortname ());
+  gdb::byte_vector result
+    = this->beneath ()->read_capability (arg0);
+  gdb_printf (gdb_stdlog, "<- %s->read_capability (", this->beneath ()->shortname ());
+  target_debug_print_CORE_ADDR (arg0);
+  gdb_puts (") = ", gdb_stdlog);
+  target_debug_print_gdb_byte_vector (result);
   gdb_puts ("\n", gdb_stdlog);
   return result;
 }