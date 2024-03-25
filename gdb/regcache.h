df26bdd94ffac8ad122e228b4c28946b10b621c8
John Baldwin <jhb@FreeBSD.org>
2024-03-25 10:49:27 -0700

    kgdb as of GDB 14
    - Add an OSABI sniffer that looks for an interpreter of "/red/herring" to
      identify a FreeBSD kernel.
    - Add per-target ops for the vmcore target to supply registers from a
      PCB and return the PCB address for a given CPU.

--- gdb/regcache.h
+++ gdb/regcache.h
@@ -134,6 +134,11 @@ regcache_map_entry_size (const struct regcache_map_entry *map)
   return size;
 }
 
+/* Same as above, but accepts a gdbarch to handle entries with a
+   variable register size.  */
+extern int regcache_map_entry_size (const struct regcache_map_entry *map,
+				    gdbarch *gdbarch);
+
 /* Transfer a set of registers (as described by REGSET) between
    REGCACHE and BUF.  If REGNUM == -1, transfer all registers
    belonging to the regset, otherwise just the register numbered
@@ -151,6 +156,13 @@ extern void regcache_collect_regset (const struct regset *regset,
 				     const struct regcache *regcache,
 				     int regnum, void *buf, size_t size);
 
+
+/* Return the offset of REGNUM in a block of registers described by an
+   array of regcache_map_entries.  If the register is not found,
+   returns -1.  */
+
+extern int regcache_map_offset (const struct regcache_map_entry *map,
+				int regnum, gdbarch *gdbarch);
 
 /* Return true if a set of registers contains the value of the
    register numbered REGNUM.  The size of the set of registers is
@@ -261,6 +273,8 @@ public:
      register left untouched.  */
   void raw_supply_part (int regnum, int offset,
 			gdb::array_view<const gdb_byte> src);
+
+  void raw_supply_unsigned (int regnum, ULONGEST val);
 
   void invalidate (int regnum);
 
