CTSRD-CHERI/gdb

commit ab54360c8391a6b5def047f1b596cab8f06f1352

riscv_parsing_subset_version: Handle "0p0" version strings.
Use a boolean to determine if a version is present (similar to
what is done in riscv_parse_prefixed_ext) rather than assuming
that both versions set to 0 means no version was present.

This fixes parsing of an attribute string of
"rv64i2p0_m2p0_a2p0_f2p0_d2p0_c2p0_xcheri0p0" which previously failed
with the error:

BFD: x ISA extension `xcheri' must be set with the versions

bfd/elfxx-riscv.c
@@ -1727,12 +1727,13 @@ riscv_parsing_subset_version (const char *p,
 			      int *major_version,
 			      int *minor_version)
 {
+  bool found_version = false;
   bool major_p = true;
   int version = 0;
   char np;
 
-  *major_version = 0;
-  *minor_version = 0;
+  *major_version = RISCV_UNKNOWN_VERSION;
+  *minor_version = RISCV_UNKNOWN_VERSION;
   for (; *p; ++p)
     {
       if (*p == 'p')
@@ -1748,21 +1749,23 @@ riscv_parsing_subset_version (const char *p,
 	  version = 0;
 	}
       else if (ISDIGIT (*p))
-	version = (version * 10) + (*p - '0');
+	{
+	  found_version = true;
+	  version = (version * 10) + (*p - '0');
+	}
       else
 	break;
     }
 
-  if (major_p)
-    *major_version = version;
-  else
-    *minor_version = version;
-
-  /* We can not find any version in string.  */
-  if (*major_version == 0 && *minor_version == 0)
+  if (found_version)
     {
-      *major_version = RISCV_UNKNOWN_VERSION;
-      *minor_version = RISCV_UNKNOWN_VERSION;
+      if (major_p)
+	{
+	  *major_version = version;
+	  minor_version = 0;
+	}
+      else
+	*minor_version = version;
     }
 
   return p;

commit 55af3a6545220d84521ffbb6d79aadb7bd3fee49

[bfd] Fix default minor version for standard extensions
Fixes: ab54360c8391a6b5def047f1b596cab8f06f1352
Assisted-By: Gemini

bfd/elfxx-riscv.c
@@ -1762,7 +1762,7 @@ riscv_parsing_subset_version (const char *p,
       if (major_p)
 	{
 	  *major_version = version;
-	  minor_version = 0;
+	  *minor_version = 0;
 	}
       else
 	*minor_version = version;